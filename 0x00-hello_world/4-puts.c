5b736479b0674c30d9e1754032c7a7534e1a5568
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-13 12:41:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- * printing a statement with puts function
- * */
+* printing a statement with puts function
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
