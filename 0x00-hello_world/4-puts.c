72e6c981d402b0049c8b935f9b1ac129d7b6169a
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-13 12:36:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*printing a statement with puts function*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
