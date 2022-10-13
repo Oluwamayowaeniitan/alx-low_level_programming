75dfb29ee970ab37721c7194353398f2aa519af8
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-13 12:12:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*a program to print out a statement using printf*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
