b8a3fb33cc53f25a421adb8f86088ccc440916d4
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-14 08:37:24 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+	putchar('\n');
+
+	return (0);
+}
+
