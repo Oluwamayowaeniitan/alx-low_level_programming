68187ed027d48f005f8bc0ba8d86fd4225af4bda
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-18 15:14:59 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always null
+ */
+void print_alphabet_x10(void)
+{
+	char (low);
+		
+		for (low = 'a'; low <= 'z'; low++)
+		{
+			putchar(low "\n" low "\n" low "\n" low "\n" low "\n" low "\n" low "\n" low "\n" low "\n" low);
+		}
+	putchar("\n");
+}
+
