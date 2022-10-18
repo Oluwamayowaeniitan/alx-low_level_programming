38d1fc8bf913c40ef91de2f69da3be15159f2147
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-18 13:39:01 -0700

    print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always null
+ */
+void print_alphabet(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+	}
+	_putchar('\n');
+}
