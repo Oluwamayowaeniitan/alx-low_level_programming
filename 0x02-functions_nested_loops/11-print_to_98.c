0a98820bef844d44ec7ccd28e2099c8b5966ca0a
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-19 08:15:43 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ *@n: starting number
+ * Description: printing n to 98
+ *
+ * Return: Always null
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	n = 'n';
+	for (i = 'n'; i = 98; i++)
+		if (n >= 1)
+			_putchar(i);
+	_putchar('\n');
+}
