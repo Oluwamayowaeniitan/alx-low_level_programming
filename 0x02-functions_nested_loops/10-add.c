4bbc6e46593e27acecc97b196a6df520f32a5a78
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-19 07:46:41 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first number
+ * @b: second number
+ * Description: addition of numbers
+ *
+ * Return: Always null
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	_putchar(sum);
+	return (0);
+}
