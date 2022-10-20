a8a16f8797dd16473536653346a2fdc32df7961c
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-20 06:42:38 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Null
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
