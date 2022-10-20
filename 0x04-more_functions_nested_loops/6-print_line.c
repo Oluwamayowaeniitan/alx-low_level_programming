5457b68d8c59b77b3f5b77d806dc77c3c9885cbe
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-20 08:06:33 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: integer
+ * Return: Null
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
