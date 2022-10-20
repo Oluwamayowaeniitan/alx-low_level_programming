125d4ef4bdcb9253dbe8b591a175af283bbcea9f
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-20 05:54:34 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: integer
+ * Return: 1 if c is a digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	int num;
+	int number = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == c)
+		{
+			number = 1;
+			break;
+		}
+	}
+	return (number);
+}
