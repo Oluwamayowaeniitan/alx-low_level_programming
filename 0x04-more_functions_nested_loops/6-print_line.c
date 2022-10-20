f1af31dc2cfd68facf46cde11d691d9b8c72805e
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-20 08:10:22 -0700

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,14 @@
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n > 0)
 	{
-		_putchar('_');
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
 	}
 }
