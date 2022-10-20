1608b9e7a54b12e4b31201e0482740b5041c1fdf
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-20 07:41:01 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Null
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (1 >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
