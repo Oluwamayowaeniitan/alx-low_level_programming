20217a313c8c32762bfb09698f32ab72afe2573d
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-26 08:18:09 -0700

    string_topper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - main function
+ * @n: input string
+ * Return: Null
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
