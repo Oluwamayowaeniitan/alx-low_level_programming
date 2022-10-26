d96b03757dcd3bd5e929b3910ce6c447743e7ab5
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-10-26 03:55:42 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - main function
+ * @dest: first character
+ * @src: second charcter
+ * @n: input
+ * Return: null
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
