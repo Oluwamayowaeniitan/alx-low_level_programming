f5c5b6be73e2ca3de35b335cf660e5c273591411
Oluwamayowaeniitan <oluwamayowaeniitan.com>
2022-11-02 14:00:05 -0700

    sqrt

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number - main function
+ * @n: integer
+ * Return: Null
+ */
+int is_prime_number(int n)
+{
+	if (n / n = 1)
+	{
+		return (1);
+	}else
+	{
+		return(0);
+	}
+}
